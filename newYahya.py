d6d9ff3be1dda32394962c82bf77c8555be618b5
Yahyakabeer <157284538+Yahyakabeer@users.noreply.github.com>
2024-02-06 21:45:18 +0500

    Add files via upload

---
+++ newYahya.py
@@ -0,0 +1,99 @@
+count = 0
+##print('Welcome to Number Guessing Game')
+
+x = max(12,3,56,100,32)
+#print(x!=99)
+
+y = min(12,3,4)
+#print(y)
+
+l = [22,32,4,45,5]
+#print(l)
+
+##for i in range(1,11):
+##    print('Hello',end=' ')
+##
+##print()#printing a new line
+###print('this is new line')
+
+##n = int(input('Enter the table no. you want to print: '))
+##y = int(input('Enter how many times you want to print the table: '))
+
+##for i in range(1,y+1):
+##    print(str(n) + ' x ' + str(i) + ' = ',n*i)
+##total = 0
+##for i in range(len(l)):
+##    total+=l[i]
+##print('the sum of your list is',total)
+
+##for i in range(30):
+##    print('*',end='')
+##print('\n   Welcome to the Caluclator')
+##
+##for i in range(30):
+##    print('*',end='')
+##print()
+##
+##
+##
+##
+##
+##def add(x,y):
+##    print(x+y)
+##    
+##def sub(x,y):
+##    print(x-y)
+##
+##def multi(x,y):
+##    print(x*y)
+##    
+##def div(x,y):
+##    print(x/y)
+##
+##
+##while True:
+##    
+##
+##    val1 = int(input('Enter the first number: '))
+##    val2 = int(input('Enter the second number: '))
+##
+##    choice = input('Enter the operation (+,-,*,/): ')
+##
+##    if choice == '+':
+##        add(val1,val2)
+##    elif choice == '-':
+##        sub(val1,val2)
+##    elif choice == '*':
+##        multi(val1,val2)
+##    elif choice == '/':
+##        div(val1,val2)
+##    else:
+##        print('ghar jao')
+##
+##    print('Press "e" to exit\nPress "c" to continue')
+##
+##    ext = input()
+##
+##    if ext.lower() == 'e':
+##        break
+##    elif ext.lower() == 'c':
+##        pass
+choose = int(input('Enter your range: '))
+for i in range(1,choose+1):
+    print('*'*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
